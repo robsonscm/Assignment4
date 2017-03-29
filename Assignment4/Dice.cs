e226601fea4e34ff8f61fb3c9a5e9b2580087584
Robson Miranda <robsonscm@gmail.com>
2017-03-29 15:24:37 -0400

    First Files

---
+++ Assignment4/Dice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Assignment4
+{
+	public class Dice
+	{
+		protected byte value;
+		private Random random;
+
+		public Dice() //Default Constructor
+		{
+			value = 1;
+			random = new Random(int.Parse(Guid.NewGuid().ToString().Substring(0, 8), System.Globalization.NumberStyles.HexNumber)); // seed it!
+		}
+	
+		public Dice(byte value) //Overload Constructor
+		{
+			this.value = value;
+			random = new Random(int.Parse(Guid.NewGuid().ToString().Substring(0, 8), System.Globalization.NumberStyles.HexNumber)); // seed it!
+		}
+
+		public void Draw()
+		{
+			Console.WriteLine(this.value);
+		}
+
+		public byte Roll()
+		{
+			//this.value = (byte) random.Next(1,7);
+			this.value = Convert.ToByte(random.Next(1, 7));
+			return this.value;
+		}
+
+		public byte GetByte() { return value; }
+	}
+}
---
+++ Assignment4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assignment4
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.DarkGray;
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.Clear();
+			Console.Title = "Dice-Arama 2001";
+
+			Dice d1 = new Dice();
+
+			for (int i = 0; i < 50; i++)
+			{
+				d1.Roll();
+				d1.Draw();
+			}
+		}
+	}
+}
